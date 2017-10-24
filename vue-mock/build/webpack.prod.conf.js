b540d08aedd68d28524837f1e1ee2c6684500a80
vincentotsai <totsai@139.com>
2017-10-24 16:15:33 +0800

    [add]yarn

--- vue-mock/build/webpack.prod.conf.js
+++ vue-mock/build/webpack.prod.conf.js
@@ -62,6 +62,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
+      favicon: './../src/assets/logo.png',
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
