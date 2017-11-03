75e5effd730784c774165fbeedb05060681eebc0
vincentotsai <totsai@139.com>
2017-11-03 16:44:21 +0800

    [add]hha

--- src/个人代码库/vue轮播卡片组件/swiper-instance.vue
+++ src/个人代码库/vue轮播卡片组件/swiper-instance.vue
@@ -70,6 +70,7 @@ export default {
   },
   // you can find current swiper instance object like this, while the notNextTick property value must be true
   // 如果你需要得到当前的swiper对象来做一些事情，你可以像下面这样定义一个方法属性来获取当前的swiper对象，同时notNextTick必须为true
+  // gcam 
   computed: {
     swiper() {
       return this.$refs.mySwiper.swiper
