f0c547fc09219510e1824b3b895ccb4287f93def
vincentotsai <totsai@139.com>
2017-11-08 20:47:40 +0800

    add:flow

--- vue-mock/src/components/NewsList.vue
+++ vue-mock/src/components/NewsList.vue
@@ -12,27 +12,47 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 export default {
-  name: "NewsList",
+  name: 'NewsList',
   data() {
     return {
       news: []
-    };
+    }
   },
   created() {
     this.fetchData()
-    log('_lodash->>',_)
+    let arr = _.chunk(['a', 'b', 'c', 'd'],2)
+    log('arr->',arr)
+
+    let arr2 = _.concat([],arr,[5,6],7,[[8]])
+    log(arr2)
+    // var saves = ['profile', 'settings']
+
+    // var done = _.after(saves.length, function() {
+    //   log('done saving!')
+    // })
+
+    // _.forEach(saves, function(type) {
+    //   log({ type: type, complete: done })
+    // })
+    // log('_lodash->>', _)
+
+    // _.delay((msg)=>{
+    //   log(msg)
+    // },1000,'hi...')
+
+
   },
   methods: {
     fetchData() {
-      axios.get("/news").then(res => {
-        log(res);
-        this.news = res.data.articles;
-      });
+      axios.get('/news').then(res => {
+        log(res)
+        this.news = res.data.articles
+      })
     }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
